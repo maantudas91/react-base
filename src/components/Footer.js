8d4764b9b8c74941bffa4106c2013510ef764db7
Maantu Das <maantudas91@gmail.com>
2018-09-14 17:07:37 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <span>Copyright © 2018. All rights reserved.</span>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
