26456596f582647c4fd13a82cda0f08bcec70f13
Maantu Das <maantudas91@gmail.com>
2018-09-19 13:20:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux';
+import LoggedinButtons from './LoggedinButtons';
+import LoggedOutButtons from './LoggedOutButtons';
+import { firebaseAuth } from '../firebase';
+import { Authorize, Logout } from '../rdx/actions/AuthActions';
+import { withRouter } from 'react-router'
 
 class Header extends Component {
+
+    constructor(props){
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+
+    logout(){
+        this.props.dispatch(Logout());
+    }
+
     render() {
+
+        const { isLoggedin, user } = this.props.user;
+        let authButton;
+        if(isLoggedin){
+            authButton = <LoggedOutButtons user={user} logout={this.logout}/>
+        }else{
+            authButton = <LoggedinButtons/>
+        }
+
         return (
             <header role="banner">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light" role="navigation">
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
@@ -17,16 +42,13 @@ class Header extends Component {
                         <li className="nav-item">
                             <NavLink className="nav-link"  activeClassName="active" to="/books">Books</NavLink>
                         </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link"  activeClassName="active" to="/profile">Profile</NavLink>
+                        </li>
                     </ul>
 
-                    <ul className="nav navbar-nav float-lg-right">
-                    <li className="nav-item">
-                            <NavLink className="nav-link"  activeClassName="active"  to="/auth/login">Login</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link"  activeClassName="active" to="/auth/signup">Signup</NavLink>
-                        </li>
-                    </ul>
+                   {authButton}
+
                 </div>
             </nav>
             </header>
@@ -34,4 +56,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default withRouter(connect(null)(Header));
