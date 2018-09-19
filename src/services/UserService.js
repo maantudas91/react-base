33ec8825a1132e715837893d6e09d9dcd632a5dc
Maantu Das <maantudas91@gmail.com>
2018-09-19 13:17:56 +0530

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -10,11 +10,18 @@ export function logout () {
 }
 
 export function login (email, pw) {
-  return firebaseAuth().signInWithEmailAndPassword(email, pw)
+  return firebaseAuth().signInWithEmailAndPassword(email, pw).then(user => storeData(user))
 }
 
 export function resetPassword (email) {
   return firebaseAuth().sendPasswordResetEmail(email)
+}
+
+export function authorize(){
+  firebaseAuth.onAuthStateChanged(function(user) {
+    if (user) console.log(user)
+      
+  });
 }
 
 export function saveUser (savedUser, ...moreInfo) {
@@ -27,4 +34,14 @@ export function saveUser (savedUser, ...moreInfo) {
       lastname : moreInfo[1]
     })
     .then(() => user)
+}
+
+
+
+function storeData(userData){
+  return new Promise(function(resolve, reject) {
+    const user = userData.user;
+    localStorage.setItem('user', JSON.stringify(user));
+    resolve(user);
+  });
 }
