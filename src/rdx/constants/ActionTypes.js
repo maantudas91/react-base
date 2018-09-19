7d3db2199a55dd074864c3b67a99264960c5185d
Maantu Das <maantudas91@gmail.com>
2018-09-19 13:16:59 +0530

    Create ActionTypes.js

---
+++ src/rdx/constants/ActionTypes.js
@@ -0,0 +1,10 @@
+/**
+ * Constants are important - they describe what type of action is performed
+ * within your app. Combined with the DevTools/logger, you can see how state and subsequently
+ * your UI is being affected.
+ */
+export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT = 'LOGOUT';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
